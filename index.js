c155125d924fe43ca8fc1adf90b0259e92c797b1
morgannegagne <morgannegagne@gmail.com>
2018-02-20 15:54:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+function bumpCounter(){
+  let counter = 0;
+
+  function addBump(){
+    counter++;
+  }
+  function getBumps(){
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType){
+  return function (deadlyDevice) {
+    return {animalType: animalType, deadlyDevice: deadlyDevice};
+  }
+}
+
+let sharkCreator = createAnimal('Shark');
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+let sharkWithFrickinCannon = sharkCreator('Cannon')
